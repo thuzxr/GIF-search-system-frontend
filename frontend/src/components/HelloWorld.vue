5dabc997e033566f5058a6421adfcc41f5bdecba
ZHANG Xurui <zxurui@outlook.com>
2019-10-08 01:14:40 +0800

    0.1 Initialize a framework of hello world, close #5

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button v-on:click="gethw">从后端获取hello world</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+axios.defaults.timeout = 5000
+
+export default {
+  data () {
+    return {
+      message: 'hello vue.js!'
+    }
+  },
+  methods: {
+    gethw: function () {
+      axios.get('http://127.0.0.1:8000/').then(response => {
+        console.log(response)
+        this.message = response.data.message
+      })
+    }
+  }
+}
+</script>
