ce7bb16b96cd460475f570066bb6d6a581cbd3ec
SigmaTsing <xushangqingxsq@126.com>
2019-10-09 21:09:29 +0800

    Remove Icon and add Img Display

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -6,23 +6,40 @@
     <div >
       <button v-on:click="search">搜索</button>
     </div>
+    <div >
+      <p>{{ImgTitle}}</p>
+    </div>
+    <div >
+      <img :src="ImgSrc">
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import picStart from '@/assets/start.gif'
+import picNotfind from '@/assets/timg.jpg'
 axios.defaults.timeout = 5000
 
 export default {
   data () {
     return {
-      text: ''
+      text: '',
+      ImgSrc: picStart,
+      ImgTitle: '点击按钮开始搜索'
     }
   },
   methods: {
     search: function () {
       axios.get('http://127.0.0.1:8000/search?key=' + this.text).then(response => {
         console.log(response)
+        if (response.data.status === 'succeed') {
+          this.ImgSrc = response.data.result[0].Oss_url
+          this.ImgTitle = response.data.result[0].Title
+        } else {
+          this.ImgSrc = picNotfind
+          this.ImgTitle = 'Oops! 找不到你想要的Gif'
+        }
       })
       console.log(this.text)
     }
