79e93b781681c8104d88f4e0bf36976b4d3e5cff
张旭睿 <2016013187@secoder.net>
2019-10-09 10:06:32 +0800

    add a search module to handle input.

---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <input v-model="text">
+    </div>
+    <div >
+      <button v-on:click="search">搜索</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+axios.defaults.timeout = 5000
+
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    search: function () {
+      axios.get('http://127.0.0.1:8000/search?key=' + this.text).then(response => {
+        console.log(response)
+      })
+      console.log(this.text)
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
     }
   ]
 })
