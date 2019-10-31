1c0336840abb75f4351e171b33b23aa903a1a8ca
陈海天 <2016010106@secoder.net>
2019-10-31 18:00:38 +0800

    feat(*): add many pages! try to use them for manage system in the future.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import NavLayout from '@/layout/NavLayout.vue'
+import DashboardLayout from '@/layout/DashboardLayout'
 
 Vue.use(Router)
 
@@ -37,6 +38,41 @@ export default new Router({
           path: '/recommend',
           name: 'recommend',
           component: () => import('@/views/Recommend.vue')
+        }
+      ]
+    },
+    {
+      path: '/dash',
+      redirect: 'dashboard',
+      component: DashboardLayout,
+      children: [
+        {
+          path: '/dashboard',
+          name: 'dashboard',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "demo" */ './views/Dashboard.vue')
+        },
+        {
+          path: '/icons',
+          name: 'icons',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Icons.vue')
+        },
+        {
+          path: '/profile',
+          name: 'profile',
+          component: () => import(/* webpackChunkName: "demo" */ './views/UserProfile.vue')
+        },
+        {
+          path: '/maps',
+          name: 'maps',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Maps.vue')
+        },
+        {
+          path: '/tables',
+          name: 'tables',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Tables.vue')
         }
       ]
     }
