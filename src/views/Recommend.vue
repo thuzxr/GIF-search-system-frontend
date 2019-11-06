a792ac62ed8bd682347d5c4d0eaec8bcf6b281d2
陈海天 <2016010106@secoder.net>
2019-11-06 23:00:47 +0800

    feat(recommend): finished the recommend page!

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <base-header class="header pb-4 pt-6 d-flex align-items-center"
+    <base-header class="header pb-md-4 pt-md-6 pt-3 d-flex align-items-center"
         style="background-position: center top;">
         <!-- Mask -->
         <span class="mask bg-gradient-success opacity-12"></span>
@@ -14,8 +14,23 @@
             </div>
         </div>
     </base-header>
-    <p v-show="err"> Oops! 找不到你想要的Gif </p>
-    <img-gallery v-bind:imgList="imgList"></img-gallery>
+    <div class="container">
+    <div class="row justify-content-center mt-5">
+      <div class="col-xl-6 col-md-8 col-10 mb-7 mb-xl-9" v-show="noImg">
+        <div class="card card-profile shadow">
+          <div class="card-body pt-0 pt-4">
+            <div class="text-center pt-1">
+              <h4>You dont have any recommend gif yet~ </h4>
+              <div class="text-center">
+                <base-button type="vue" class="my-0" @click.stop="recommend">get some</base-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img-gallery v-bind:imgList="imgList"></img-gallery>
+    </div>
+    </div>
   </div>
 </template>
 <script>
@@ -30,31 +45,35 @@ export default {
       err: false
     }
   },
-  mounted () {
-    this.$api.recommend(this.imgName).then(response => {
-      if (response.status === 'succeed') {
-        this.err = false
-        var list = response.result
-        this.imgList = list.map(function (item) {
-          var t = {
-            title: item.Title,
-            url: item.Oss_url,
-            thumbnail: item.Oss_url
-          }
-          return t
-        })
-      } else {
-        this.err = true
-        this.imgList = [{
-          title: 'Oops! 找不到你想要的Gif',
-          url: '../assets/timg.jpg',
-          thumbnail: '../assets/timg.jpg'
-        }]
-      }
-    })
+  computed: {
+    noImg () {
+      return (this.err || !this.imgList || this.imgList.length === 0)
+    }
   },
   methods: {
-
+    recommend: function () {
+      this.$api.recommend(this.imgName).then(response => {
+        if (response.status === 'succeed') {
+          this.err = false
+          var list = response.result
+          this.imgList = list.map(function (item) {
+            var t = {
+              title: item.Title,
+              url: item.Oss_url,
+              thumbnail: item.Oss_url
+            }
+            return t
+          })
+        } else {
+          this.err = true
+          this.imgList = [{
+            title: 'Oops! 找不到你想要的Gif',
+            url: '../assets/timg.jpg',
+            thumbnail: '../assets/timg.jpg'
+          }]
+        }
+      })
+    }
   },
   components: {
     'img-gallery': ImgGallery
