96a3792151c0446038f45380b01e975ad77f2fec
梁念宁 <liangnn17@mails.tsinghua.edu.cn>
2019-11-12 11:04:06 +0800

    feat(test): add jest dependency

--- tests/unit/search.spec.js
+++ tests/unit/search.spec.js
@@ -1,6 +1,18 @@
-describe('Search.vue', () => {
-  it('renders img div when passed', () => {
-    var t = true
-    expect(t).toBe(true)
+import Vue from 'vue'
+import Search from '@/views/Search'
+
+describe('Search', () => {
+  // 检查原始组件选项
+  it('has a data hook', () => {
+    expect(typeof Search.data).toBe('function')
+    var data = Search.data()
+    expect(data.err).toBe(false)
+  })
+
+  it('correctly sets the message when created', () => {
+    const vm = new Vue(Search).$mount()
+    expect(vm.err).toBe(false)
+    expect(vm.imgList.length).toBe(0)
   })
 })
+
